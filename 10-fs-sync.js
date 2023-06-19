8b2d6d4a79ef30315e2ffcb4becf52d40d19c764
JKS5 <jokekhanson@gmail.com>
2023-06-19 21:16:55 +0900

    [Node]- async

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -1,6 +1,6 @@
 const { readFileSync, writeFileSync } = require("fs");
 // ! fs 모듈이 가진 함수로, 동기적으로 읽고 쓰기 가능
-
+console.log("start");
 const first = readFileSync("./content/first.txt", "utf8");
 const second = readFileSync("./content/second.txt", "utf8");
 
@@ -14,3 +14,8 @@ writeFileSync(
 );
 
 // ! writeFileSync(파일 쓸 경로 , 작성할 파일 내용, 옵션 :파일 작성시 동작제어 가능 위의 경우 내용을 추가(append)함. )
+
+console.log("done with this task");
+console.log("starting the next one ");
+
+//! sync이기 때문에 순서대로 나온다.
--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,5 +1,7 @@
 const { readFile, writeFile } = require("fs");
 
+console.log("start");
+
 readFile("./content/first.txt", (err, result) => {
   if (err) {
     console.log(err);
@@ -20,8 +22,11 @@ readFile("./content/first.txt", (err, result) => {
           console.log(err);
           return;
         }
-        console.log(result);
+        console.log("done with thist test");
       }
     );
   });
 });
+console.log("starting next task");
+
+//! start -> starting next task -> done with this test , console.log 찍으면 async 하게 작동하기 때문에 시간 절약이 된다.
